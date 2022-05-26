38e1bc8c7659a2cb95685f0d9767c79ca78171f1
Your Name <applezafar@gmail.com>
2022-05-27 00:22:51 +1000

    Homework6

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,28 @@ public class Main {
 
     public static void main(String[] args) {
 
-    String firstName = "Ivan";
-    String middleName = "Ivanovich";
-    String lastName = "Ivanov";
-    String fullName = "Ivanov" +  "Ivan" +  "Ivanovich";
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = "Ivanov" + "Ivan" + "Ivanovich";
         System.out.println("fullName = " + fullName);
 
-       String fullName1 = "Ivanov Ivan Ivanovich";
+        String fullName1 = "Ivanov Ivan Ivanovich";
         System.out.println("Data full name of the employee to fill out the report" + fullName1.toUpperCase());
+
         String fullName2 = "Ivanov Ivan Ivanovich";
-        System.out.println("Employee full name data for administrative department" + fullName2.split(";"));}}
+        System.out.println("Employee full name data for administrative department" + fullName2.replace(" ", ";"));
+
+        String fullName3 = "Иванов Семён Семёнович";
+        System.out.println(fullName3.replace('ё','е'));
+    }
+}
+
+
+
+
+
+
 
 
 
