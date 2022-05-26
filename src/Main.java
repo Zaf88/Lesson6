2c811c8d6f92ecfb65c567303f300d16b9917816
Your Name <applezafar@gmail.com>
2022-05-26 22:52:05 +1000

    Homework6

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Main {
-}
+public class Main {
+
+
+    public static void main(String[] args) {
+
+    String firstName = "Ivan";
+    String middleName = "Ivanovich";
+    String lastName = "Ivanov";
+    String fullName = "Ivanov" +  "Ivan" +  "Ivanovich";
+        System.out.println("fullName = " + fullName);
+
+       String fullName1 = "Ivanov Ivan Ivanovich";
+        System.out.println("Data full name of the employee to fill out the report" + fullName1.toUpperCase());
+        String fullName2 = "Ivanov Ivan Ivanovich";
+        System.out.println("Employee full name data for administrative department" + fullName2.split(";"));}}
+
+
+
+
